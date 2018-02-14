3c6d04cecd587a583d7e26b71134e60a6ab802fa
rgr88 <31260517+rgr88@users.noreply.github.com>
2018-02-13 20:13:29 -0500

    Update client.js

--- public/client.js
+++ public/client.js
@@ -1,13 +1,10 @@
 /*
 client.js
-
 Author: Nikolas Martelaro (nmartelaro@gmail.com)
 Extended: David Goeicke (da.goedicke@gmail.com)
 Purpose: This run the interactivity and communication for the web app. This file
 is served to the users web browser and executes on the browser.
-
 Usage: This file is called automatically when the webpage is served.
-
 //--Addition. Added a button handling for the `Take a picture` button.
 */
 
@@ -25,14 +22,19 @@ function ledOFF() {
 }
 
 //-- Addition: Forward the `Take a picture` button-press to the webserver.
-function takePicture(){
-  socket.emit('takePicture');
+function takePicture(feat){
+  socket.emit('takePicture', feat);
 }
 
 //-- Addition: This function receives the new image name and applies it to html element.
 
 socket.on('newPicture', function(msg) {
   document.getElementById('pictureContainer').src=msg;
+});
+
+socket.on('twoPictures', function(imgs){
+  document.getElementById('pictureContainer').src = imgs[0];
+  document.getElementById('alteredImg').src = imgs[1];
 });
 // read the data from the message that the server sent and change the
 // background of the webpage based on the data in the message
